699779778ff55e75f4e4efc95849c7d1b5aaf8fd
MagicBot-Inc <magicbot@inbox.ru>
2019-10-02 22:14:51 +0300

    Updates

---
+++ precache-manifest.40e80486a0cb46e360440dff49041820.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b615945fc5c5b1bc5bb",
+    "url": "/ecomerce-app/static/js/main.d95ff946.chunk.js"
+  },
+  {
+    "revision": "9d4188da6b21417c0d6b",
+    "url": "/ecomerce-app/static/js/runtime~main.b818b5c3.js"
+  },
+  {
+    "revision": "0a6256c803087488d157a0d08d6cb7ad",
+    "url": "/ecomerce-app/index.html"
+  }
+];
